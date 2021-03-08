ec14de9170139b7dfbaeb45b016c6a56add6d658
j-m-wegmann <69283761+j-m-wegmann@users.noreply.github.com>
2021-03-08 14:20:29 -0600

    Added code for the highpass filter

--- ese351_casestudy1.m
+++ ese351_casestudy1.m
@@ -6,7 +6,6 @@
 fs = xvfs; 
 sound(xv,fs)
 
-
 %% compute and plot fft
 
 f = [0:length(xv)-1]*fs/length(xv);
@@ -17,10 +16,26 @@ ylabel('|X(f)|')
 
 %%
 
-f = 10;
+f = 100;
 w = 2*pi*f;
+delta_t = 1/44100;
 t_new = (0:delta_t:0.2);
 x = exp(j*w*t_new);
+R = 1000;
+C = 0.00002;
 
-%calculate output for low pass filter
+% calculate output for low pass filter
 y_low = lsim(1/(R*C),[1 1/(R*C)],x,t_new);
+figure
+hold on;
+plot(t_new,x);
+plot(t_new,y_low);
+hold off;
+
+% calculate output for highpass filter 
+y_high = lsim([1 0],[1 1/(R*C)], x, t_new);
+figure
+hold on;
+plot(t_new,x);
+plot(t_new,y_high);
+hold off
