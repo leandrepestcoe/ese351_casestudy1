6519c0c21b0eddd8fb3789f2c2c7c37447143e48
leandrepestcoe <54565087+leandrepestcoe@users.noreply.github.com>
2021-03-10 14:32:43 -0600

    fixed bands

--- CaseStudy1_Task1.m
+++ CaseStudy1_Task1.m
@@ -17,66 +17,69 @@ figure, plot(f,P1);
 xlabel('f, Hz')
 ylabel('|X(f)|')
 
-%% define bandpass filter with 3 freq bands
+%% define bandpass filter with 5 freq bands
 
 delta_t = 0.1;
 t_new = (0:delta_t:(length(xv)-1)*delta_t)'; %define time vector
 x = xv; %set input to audio signal
 
 % bandpass filter 1
-%R_low1 = 1000;
-%C_low1 = 0.00002;
+R_low1 = 1000;
+C_low1 = 7.96e-7; %200 Hz
 R_high1 = 1000;
-C_high1 = 7.96e-7; %200 Hz
-%y_low1 = lsim(1/(R_low1*C_low1),[1 1/(R_low1*C_low1)],x,t_new);
+C_high1 = 3.32e-7; %480 Hz
+y_low1 = lsim(1/(R_low1*C_low1),[1 1/(R_low1*C_low1)],x,t_new);
 y_high1 = lsim([1 0],[1 1/(R_high1*C_high1)], x, t_new);
-%y_band1 = y_low1+y_high1;
+y_band1 = y_low1+y_high1;
 
 % bandpass filter 2
 R_low2 = 1000;
-C_low2 = 7.96e-7; %200 Hz
+C_low2 = 3.32e-7; %480 Hz
 R_high2 = 1000;
-C_high2 = 9.95e-8; %1600 Hz
+C_high2 = 2.09e-7; %760 Hz
 y_low2 = lsim(1/(R_low2*C_low2),[1 1/(R_low2*C_low2)],x,t_new);
 y_high2 = lsim([1 0],[1 1/(R_high2*C_high2)], x, t_new);
 y_band2 = y_low2+y_high2;
 
 % bandpass filter 3
 R_low3 = 1000;
-C_low3 = 9.95e-8; %1600 Hz
-%R_high3 = 1000;
-%C_high3 = 0.00002;
+C_low3 = 2.09e-7; %760 Hz
+R_high3 = 1000;
+C_high3 = 1.53e-7; %1040 Hz
 y_low3 = lsim(1/(R_low3*C_low3),[1 1/(R_low3*C_low3)],x,t_new);
-%y_high3 = lsim([1 0],[1 1/(R_high3*C_high3)], x, t_new);
-%y_band3 = y_low3+y_high3;
+y_high3 = lsim([1 0],[1 1/(R_high3*C_high3)], x, t_new);
+y_band3 = y_low3+y_high3;
+
+% bandpass filter 4
+
+R_low4 = 1000;
+C_low4 = 1.53e-7; %1040 Hz
+R_high4 = 1000;
+C_high4 = 1.21e-7; %1320 Hz
+y_low4 = lsim(1/(R_low4*C_low4),[1 1/(R_low4*C_low4)],x,t_new);
+y_high4 = lsim([1 0],[1 1/(R_high4*C_high4)], x, t_new);
+y_band4 = y_low4+y_high4;
+
+% bandpass filter 5
+
+R_low5 = 1000;
+C_low5 = 1.21e-7; %1320 Hz
+R_high5 = 1000;
+C_high5 = 9.95e-8; %1600 Hz
+y_low5 = lsim(1/(R_low5*C_low5),[1 1/(R_low5*C_low5)],x,t_new);
+y_high5 = lsim([1 0],[1 1/(R_high5*C_high5)], x, t_new);
+y_band5 = y_low5+y_high5;
+
 
 % final filter
-gain1 = 0.01;
+gain1 = 1;
 gain2 = 1;
-gain3 = 0.01;
-final_filter = (gain1*y_high1)+(gain2*y_band2)+(gain3*y_low3); %three bandpass filters in parallel
-sound(final_filter,fs);
+gain3 = 1;
+gain4 = 1;
+gain5 = 1;
+final_filter = (gain1*y_high1)+(gain2*y_band2)+(gain3*y_band3)+(gain4*y_band4)+(gain5*y_band5); %five bandpass filters in parallel
+%sound(final_filter,fs);
 
-
-%% code for other bands (if needed)
-
-% % bandpass filter 4
-% 
-% R_low4 = 1000;
-% C_low4 = 0.00002;
-% R_high4 = 1000;
-% C_high4 = 0.00002;
-% y_low4 = lsim(1/(R_low4*C_low4),[1 1/(R_low4*C_low4)],x,t_new);
-% y_high4 = lsim([1 0],[1 1/(R_high4*C_high4)], x, t_new);
-% y_band4 = y_low4+y_high4;
-% 
-% % bandpass filter 5
-% 
-% R_low5 = 1000;
-% C_low5 = 0.00002;
-% R_high5 = 1000;
-% C_high5 = 0.00002;
-% y_low5 = lsim(1/(R_low5*C_low5),[1 1/(R_low5*C_low5)],x,t_new);
-% y_high5 = lsim([1 0],[1 1/(R_high5*C_high5)], x, t_new);
-% y_band5 = y_low5+y_high5;
+freq_response = freqs(final_filter,x,round(2*pi*60));
+plot(freq_response);
 
