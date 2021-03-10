69c1ea903ba7ad6f2c8e8e265b63dc69aea1bdb9
leandrepestcoe <54565087+leandrepestcoe@users.noreply.github.com>
2021-03-09 18:50:26 -0600

    trying to pass audio through filter

--- ese351_casestudy1.m
+++ ese351_casestudy1.m
@@ -20,22 +20,27 @@ f = 100;
 w = 2*pi*f;
 delta_t = 1/44100;
 t_new = (0:delta_t:0.2);
-x = exp(j*w*t_new);
-R = 1000;
-C = 0.00002;
+%x = exp(j*w*t_new);
+x = xv; %input is audio data...
+R_low = 1000;
+C_low = 0.00002;
+R_high = 1000;
+C_high = 0.00002;
 
 % calculate output for low pass filter
-y_low = lsim(1/(R*C),[1 1/(R*C)],x,t_new);
+y_low = lsim(1/(R_low*C_low),[1 1/(R_low*C_low)],x,t_new);
 figure
 hold on;
 plot(t_new,x);
 plot(t_new,y_low);
+legend('input','output');
 hold off;
 
 % calculate output for highpass filter 
-y_high = lsim([1 0],[1 1/(R*C)], x, t_new);
+y_high = lsim([1 0],[1 1/(R_high*C_high)], x, t_new);
 figure
 hold on;
 plot(t_new,x);
 plot(t_new,y_high);
+legend('input','output');
 hold off
